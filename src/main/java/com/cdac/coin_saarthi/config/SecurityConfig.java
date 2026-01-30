616ce6b2bc85e5be450468a6e9da9f11072d05e6
hjadhav2k3 <hanumanj2k3@gmail.com>
2026-01-30 22:59:34 +0530

    Cors Config

--- src/main/java/com/cdac/coin_saarthi/config/SecurityConfig.java
+++ src/main/java/com/cdac/coin_saarthi/config/SecurityConfig.java
@@ -1,7 +1,10 @@
 package com.cdac.coin_saarthi.config;
 
 import org.springframework.context.annotation.Bean;
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -18,13 +21,16 @@ public class SecurityConfig{
 	
 	@Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
+      
         http
+        .cors(withDefaults())
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
+            		.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .requestMatchers(
                     "/api/auth/**",
                     "/swagger-ui/**",
+                    "/crypto/**",   
                     "/v3/api-docs/**"
                 ).permitAll()
                 .anyRequest().authenticated()
